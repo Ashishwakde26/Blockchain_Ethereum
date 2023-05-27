02efeba7e83d16d184501dc08daa952debb3720f
Ashishwakde26 <Ashish261088@gmail.com>
2023-05-27 08:39:55 +0530

    Updates

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,10 +1,30 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-class Car {
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!']})
+    .send({ from : accounts[0], gas: '1000000'});
+});
+
+
+describe('Inbox', ()=> {
+    it('deploys a contract', () => {
+      assert.ok(inbox.options.address)
+    });
+    
+});
+
+/* class Car {
     park() {
         return "stopped";
     }
@@ -30,4 +50,4 @@ describe('Car', () => {
         const car = new Car();
         assert.equal(car.drive(), 'vroom');
     })
-});
+}); */
